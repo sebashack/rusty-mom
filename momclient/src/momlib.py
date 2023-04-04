ad02d3ca39cf075cf75aa5a3c71496c0f2cdcb27
sebashack <sebastian0092@gmail.com>
2023-04-03 20:22:54 -0500

    Add try catches

--- momclient/src/momlib.py
+++ momclient/src/momlib.py
@@ -35,11 +35,15 @@ class MoMClient:
         response = requests.put(
             f"{self.root()}/queues/{queue_label}/channels/{topic}", headers=headers
         )
-        data = response.json()
-
         print(f"Response Status: {response.status_code}")
-        print(f"Response body: {data}")
-        return (MoMInfo(data["host"], data["port"]), Channel(data["id"]))
+
+        try:
+            data = response.json()
+            print(f"Response body: {data}")
+
+            return (MoMInfo(data["host"], data["port"]), Channel(data["id"]))
+        except:
+            return None
 
     def delete_channel(self, channel_id):
         response = requests.delete(f"{self.root()}/channels/{channel_id}")
@@ -48,21 +52,27 @@ class MoMClient:
 
     def list_queues(self):
         response = requests.get(f"{self.root()}/queues")
-        data = response.json()
-
         print(f"Response Status: {response.status_code}")
-        print(f"Response body: {data}")
 
-        return data
+        try:
+            data = response.json()
+            print(f"Response body: {data}")
+
+            return data
+        except:
+            return None
 
     def list_channels(self):
         response = requests.get(f"{self.root()}/channels")
-        data = response.json()
-
         print(f"Response Status: {response.status_code}")
-        print(f"Response body: {data}")
 
-        return data
+        try:
+            data = response.json()
+            print(f"Response body: {data}")
+
+            return data
+        except:
+            return None
 
     # Helpers
     def root(self):
