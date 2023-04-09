b501f1dccad82dce88a4cbdaa30ad826dda98255
DavidLakeT <djcardonan@eafit.edu.co>
2023-04-09 18:36:34 -0500

    Added comment pusher & consumer

--- momclient/news-consumer/__main__.py
+++ momclient/news-consumer/__main__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 import sys
 import threading
 import time
@@ -11,11 +11,27 @@ from momlib import MoMClient, Pusher, Subscriber
 
 RETRY_DELAY = 2
 MAX_ATTEMPTS = 5
+PUSH_DELAY_SECS = 5
 
 app = Flask(__name__)
 message_list = []
 message_lock = threading.Lock()
 
+
+def create_queue_pusher(mom_client, queue_label, retry_delay_secs, max_attempts):
+    def retry(i):
+        if i >= max_attempts:
+            raise Exception("Could not establish connection with MoM Server")
+        else:
+            print(f"Connecting pusher to queue, attempt {i + 1}")
+            try:
+                _, info = mom_client.get_queue_info(queue_label)
+                return Pusher(info)
+            except:
+                time.sleep(retry_delay_secs)
+                return retry(i + 1)
+
+    return retry(0)
 
 @app.route("/news")
 def get_messages():
@@ -52,14 +68,31 @@ def consume_with_retry(mom_client, queue_label, topic, retry_delay_secs, max_att
 
 def main():
     mom_client = MoMClient("127.0.0.1", 8082)
+    mom_client.create_queue("comments-queue")
+
+    def push_news():
+        pusher = create_queue_pusher(
+            mom_client, "comments-queue", RETRY_DELAY, MAX_ATTEMPTS
+        )
+        while True:
+            now = str(datetime.now())
+            msg = f"Comments with time {now}".encode("utf-8")
+            try:
+                pusher.push(msg, "comments-queue", topic="comments")
+            except:
+                pusher = create_queue_pusher(
+                    mom_client, "comments-queue", RETRY_DELAY, MAX_ATTEMPTS
+                )
+            time.sleep(PUSH_DELAY_SECS)
 
     def consume_news():
         consume_with_retry(mom_client, "news-queue", "news", RETRY_DELAY, MAX_ATTEMPTS)
 
+    threading.Thread(target=push_news).start()
     threading.Thread(target=consume_news).start()
 
     app.config["messages_list"] = message_list
-    app.run()
+    app.run(port=5001)
 
 
 if __name__ == "__main__":
--- momclient/news-pusher/__main__.py
+++ momclient/news-pusher/__main__.py
@@ -3,14 +3,48 @@ import sys
 import threading
 import time
 
+from flask import Flask, render_template
+
 sys.path.insert(1, "lib")
 
 from momlib import MoMClient, Pusher, Subscriber
 
-RETRY_DELAY_SECS = 2
+
+RETRY_DELAY = 2
 MAX_ATTEMPTS = 5
 PUSH_DELAY_SECS = 5
 
+app = Flask(__name__)
+message_list = []
+message_lock = threading.Lock()
+
+@app.route("/comments")
+def get_messages():
+    messages = app.config["messages_list"]
+    return render_template('comments.html', messages=messages)
+
+def on_message(message):
+    with message_lock:
+        if message["id"] not in map(lambda m: m["id"], message_list):
+            message_list.append({"id": message["id"], "content": message["content"].decode("UTF-8")})
+
+def consume_with_retry(mom_client, queue_label, topic, retry_delay_secs, max_attempts):
+    def retry(i):
+        k = i
+        if i > max_attempts:
+            raise Exception("Could not establish connection with MoM Server")
+        else:
+            print(f"Connecting consumer, attempt {i + 1}")
+            try:
+                mom_info, channel_info = mom_client.create_channel(queue_label, topic)
+                subscriber = Subscriber(mom_info, channel_info)
+                k = -1
+                subscriber.consume(on_message)
+            except Exception as e:
+                time.sleep(retry_delay_secs)
+                return retry(k + 1)
+
+    return retry(0)
 
 def create_queue_pusher(mom_client, queue_label, retry_delay_secs, max_attempts):
     def retry(i):
@@ -27,7 +61,6 @@ def create_queue_pusher(mom_client, queue_label, retry_delay_secs, max_attempts)
 
     return retry(0)
 
-
 def main():
     mom_client = MoMClient("127.0.0.1", 8082)
     mom_client.create_queue("news-queue")
@@ -36,7 +69,7 @@ def main():
 
     def push_news():
         pusher = create_queue_pusher(
-            mom_client, "news-queue", RETRY_DELAY_SECS, MAX_ATTEMPTS
+            mom_client, "news-queue", RETRY_DELAY, MAX_ATTEMPTS
         )
         while True:
             now = str(datetime.now())
@@ -45,15 +78,18 @@ def main():
                 pusher.push(msg, "news-queue", topic="news")
             except:
                 pusher = create_queue_pusher(
-                    mom_client, "news-queue", RETRY_DELAY_SECS, MAX_ATTEMPTS
+                    mom_client, "news-queue", RETRY_DELAY, MAX_ATTEMPTS
                 )
             time.sleep(PUSH_DELAY_SECS)
 
+    def consume_comments():
+        consume_with_retry(mom_client, "comments-queue", "comments", RETRY_DELAY, MAX_ATTEMPTS)
+
     threading.Thread(target=push_news).start()
+    threading.Thread(target=consume_comments).start()
 
-    while True:
-        time.sleep(10)
-
+    app.config["messages_list"] = message_list
+    app.run()
 
 if __name__ == "__main__":
     main()
