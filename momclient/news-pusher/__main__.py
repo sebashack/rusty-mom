284b1700e2f34a62478f428f472a1213bc3cedb5
sebashack <sebastian0092@gmail.com>
2023-04-09 13:49:20 -0500

    Refactor news-pusher

--- momclient/news-pusher/__main__.py
+++ momclient/news-pusher/__main__.py
@@ -7,34 +7,43 @@ sys.path.insert(1, "lib")
 
 from momlib import MoMClient, Pusher, Subscriber
 
+RETRY_DELAY_SECS = 2
+MAX_ATTEMPTS = 5
+
+
+def restablish_queue_pusher(mom_client, queue_label, retry_delay_secs, max_attempts):
+    def retry(i):
+        if i >= max_attempts:
+            raise Exception("Could not restablish connection with MoM Server")
+        else:
+            try:
+                _, info = mom_client.get_queue_info(queue_label)
+                return Pusher(info)
+            except:
+                time.sleep(retry_delay_secs)
+                return retry(i + 1)
+
+    return retry(0)
+
 
 def main():
-    retry_delay = 3
-    max_attempts = 3
     mom_client = MoMClient("127.0.0.1", 8082)
-    mom_client.create_queue("news-queue", retry_delay, max_attempts)
+    mom_client.create_queue("news-queue", RETRY_DELAY, MAX_ATTEMPTS)
 
-    _, mom_info = mom_client.get_queue_info("news-queue", retry_delay, max_attempts)
+    _, mom_info = mom_client.get_queue_info("news-queue", RETRY_DELAY, MAX_ATTEMPTS)
 
     def push_news():
         pusher = Pusher(mom_info)
         while True:
-            attempts = 0
-            while attempts < max_attempts:
-                now = str(datetime.now())
-                msg = f"News with time {now}".encode("utf-8")
-                try:
-                    pusher.push(msg, "news-queue", topic="news")
-                    attempts = 0
-                except:
-                    if attempts == max_attempts-1:
-                        print("Max retries reached")
-                        exit()
-                    time.sleep(retry_delay)
-                    attempts += 1
-                    _, info = mom_client.get_queue_info("news-queue", retry_delay, max_attempts)
-                    pusher = Pusher(info)
-                time.sleep(5)
+            now = str(datetime.now())
+            msg = f"News with time {now}".encode("utf-8")
+            try:
+                pusher.push(msg, "news-queue", topic="news")
+            except:
+                pusher = restablish_queue_pusher(
+                    mom_client, "news-queue", RETRY_DELAY_SECS, MAX_ATTEMPTS
+                )
+            time.sleep(5)
 
     threading.Thread(target=push_news).start()
 
