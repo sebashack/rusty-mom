c4c2ac90b6a7624865c4a7c131cd0308b886ec0f
sebashack <sebastian0092@gmail.com>
2023-04-09 14:25:21 -0500

    Refactor news consumer

--- momclient/news-consumer/__main__.py
+++ momclient/news-consumer/__main__.py
@@ -1,29 +1,46 @@
 from datetime import datetime
+from flask import Flask, jsonify
 import sys
-import time
 import threading
+import time
 
 sys.path.insert(1, "lib")
 
 from momlib import MoMClient, Pusher, Subscriber
+from server import app
 
+
+RETRY_DELAY = 2
+MAX_ATTEMPTS = 5
 
 message_list = []
-mutex = threading.Lock()
+message_lock = threading.Lock()
+
+
+@app.route("/news")
+def get_messages():
+    messages = app.config["messages_list"]
+    return jsonify(messages)
 
 
 def on_message(message):
-    with mutex:
+    with message_lock:
         message_list.append(message)
 
-def main():
-    retry_delay = 2
-    max_attempts = 5
-    mom_client = MoMClient("127.0.0.1", 8082)
-    mom_info, channel_info = mom_client.create_channel("news-queue", retry_delay, max_attempts, "news")
 
-    subscriber = Subscriber(mom_info, channel_info)
-    subscriber.consume(on_message)
+def main():
+    mom_client = MoMClient("127.0.0.1", 8082)
+    mom_info, channel_info = mom_client.create_channel("news-queue", topic="news")
+
+    def consume_news():
+        subscriber = Subscriber(mom_info, channel_info)
+        subscriber.consume(on_message)
+
+    threading.Thread(target=consume_news).start()
+
+    app = Flask(__name__)
+    app.config["messages_list"] = message_list
+    app.run()
 
 
 if __name__ == "__main__":
